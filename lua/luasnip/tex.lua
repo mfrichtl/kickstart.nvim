d3fe1d46836af5f5a2cf5d4204bf930b0b274fc1
Matt Frichtl <4640334+mfrichtl@users.noreply.github.com>
2025-01-06 18:12:00 -0500

    More latex snippets.

--- lua/luasnip/tex.lua
+++ lua/luasnip/tex.lua
@@ -22,7 +22,11 @@ ls.add_snippets('tex', {
       i(1, 'Maths go here.')
     )),
   }),
-  -- symbols
+  -- Maths symbols
+  s({ trig = ';deg', dscr = 'Degree symbol.' }, {
+    t '^\\circ',
+  }),
+  -- Greek letters
   s({ trig = ';a', dscr = 'Greek lower-case letter alpha' }, {
     t '\\alpha',
   }),
@@ -35,8 +39,32 @@ ls.add_snippets('tex', {
   s({ trig = ';e', dscr = 'Greek lower-case letter epsilon' }, {
     t '\\epsilon',
   }),
-  s({ trig = ';Ms', dscr = 'Insert martensite start temperature.' }, {
-    t 'M_\\text{s}',
+  -- General materials science stuffs
+  s({ trig = ';Gm', dscr = 'Molar Gibbs energy' }, {
+    t 'G_\\text{m}',
+  }),
+  s({ trig = ';k2s2o5', dscr = 'Potassium metabisulfite' }, {
+    t '\\BPChem{K\\_2S\\_2O\\_5}',
+  }),
+  s({ trig = ';na2s2o5', dscr = 'Sodium metabisulfite' }, {
+    t '\\BPChem{Na\\_2S\\_2O\\_5}',
+  }),
+  -- Requires defining \wtpercent in siunitx and BPChem package
+  s({ trig = ';wt%', dscr = 'Element by weight percent' }, {
+    unpack(fmta('\\qty{<1>}{\\wtpercent}~\\BPChem{<2>}', { i(1), i(2) })),
+  }),
+  -- steel-related symbols
+  s({ trig = ';Ac1', dscr = 'Ac1 Temperature.' }, {
+    t 'A_\\text{c1}',
+  }),
+  s({ trig = ';Ac3', dscr = 'Ac3 Temperature.' }, {
+    t 'A_\\text{c3}',
+  }),
+  s({ trig = ';Ae1', dscr = 'Ae1 Temperature.' }, {
+    t 'A_\\text{e1}',
+  }),
+  s({ trig = ';Ae3', dscr = 'Ae3 Temperature.' }, {
+    t 'A_\\text{e3}',
   }),
   s({ trig = ';DGa', dscr = 'Driving Force for lath/plate martensite.' }, {
     t '\\Delta G_\\text{m}^{\\alpha \\rightarrow \\gamma}',
@@ -44,8 +72,14 @@ ls.add_snippets('tex', {
   s({ trig = ';DGe', dscr = 'Driving Force for epsilon martensite.' }, {
     t '\\Delta G_\\text{m}^{\\epsilon \\rightarrow \\gamma}',
   }),
-  s({ trig = ';deg', dscr = 'Degree symbol.' }, {
-    t '^\\circ',
+  s({ trig = ';M7C3', dscr = 'Insert M7C3 (carbide).' }, {
+    t '\\BPChem{M\\_7C\\_3}',
+  }),
+  s({ trig = ';M23C6', dscr = 'Insert M23C6 (carbide).' }, {
+    t '\\BPChem{M\\_{23}C\\_6',
+  }),
+  s({ trig = ';Ms', dscr = 'Insert martensite start temperature.' }, {
+    t 'M_\\text{s}',
   }),
 
   -- figures
@@ -61,7 +95,7 @@ ls.add_snippets('tex', {
         \small\normalsize
         \begin{quote}
             \caption{<3>}
-            \label{<4>}
+            \label{figure:<4>}
         \end{quote}
         \end{figure}
         \renewcommand{\baselinestretch}{2}
@@ -94,5 +128,11 @@ ls.add_snippets('tex', {
       ]],
       { i(1), i(2), i(3), i(4), i(5) }
     )),
+  }),
+  s({ trig = ';tn', dscr = 'Insert siunitx tablenum.' }, {
+    unpack(fmta('\\tablenum{<1>}', { i(1) })),
+  }),
+  s({ trig = ';tq', dscr = 'Insert siunitx tableqty.' }, {
+    unpack(fmta('\\tableqty{<1>}{<2>}', { i(1), i(2) })),
   }),
 })
